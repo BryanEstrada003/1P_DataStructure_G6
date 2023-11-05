5b46f0ade57280186d09a222917c5dbd5ca528f8
BryanEstrada003 <mbestrad@espol.edu.ec>
2023-11-05 10:53:20 -0500

    Modificaciones en TestProject
    Prueba de cómo sería añadir un link y se muestre por pantalla y cargar imágenes y estas se muestre por pantalla

--- TestProject/src/main/java/com/mycompany/testproject/App.java
+++ TestProject/src/main/java/com/mycompany/testproject/App.java
@@ -2,19 +2,27 @@ package com.mycompany.testproject;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
+    
+    private ImageView imageView;
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Mini Aplicación JavaFX para URL");
+        primaryStage.setTitle("Mini Aplicación JavaFX para URL e Imagen");
 
         // Crear un cuadro de texto para ingresar una URL
         TextField textField = new TextField();
@@ -22,6 +30,25 @@ public class App extends Application {
 
         // Crear un elemento de texto para mostrar la URL como enlace
         Text linkText = new Text();
+
+        
+
+        // Crear un botón para cargar una imagen
+        Button loadImageButton = new Button("Cargar Imagen");
+
+        // Crear un ImageView para mostrar la imagen
+        imageView = new ImageView();
+
+        // Evento para cargar una imagen desde un archivo
+        loadImageButton.setOnAction(event -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Imágenes", "*.jpg", "*.jpeg", "*.png", "*.gif"));
+            File selectedFile = fileChooser.showOpenDialog(primaryStage);
+            if (selectedFile != null) {
+                Image image = new Image(selectedFile.toURI().toString());
+                imageView.setImage(image);
+            }
+        });
 
         // Evento para actualizar el elemento de texto con la URL ingresada
         textField.setOnAction(event -> {
@@ -38,10 +65,10 @@ public class App extends Application {
 
         // Crear un diseño de caja vertical para organizar los elementos
         VBox vbox = new VBox(10);
-        vbox.getChildren().addAll(textField, linkText);
+        vbox.getChildren().addAll(textField, linkText, loadImageButton, imageView);
 
         // Crear una escena y mostrarla en el escenario
-        Scene scene = new Scene(vbox, 300, 80);
+        Scene scene = new Scene(vbox, 400, 300);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
