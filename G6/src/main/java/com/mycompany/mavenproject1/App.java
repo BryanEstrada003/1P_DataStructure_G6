8b291ba752da03fe7c646596c63e4b4caa8ee9d3
aszurita <nzuritag@fiec.espol.edu.ec>
2023-11-12 00:57:30 -0500

    verificar login

--- G6/src/main/java/com/mycompany/mavenproject1/App.java
+++ G6/src/main/java/com/mycompany/mavenproject1/App.java
@@ -21,7 +21,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("contacts"));
+        scene = new Scene(loadFXML("login"));
         stage.setScene(scene);
         stage.show();
     }
