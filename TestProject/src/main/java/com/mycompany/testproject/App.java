9f73a6f2a7d29eea5cac663fef8a4dcfb2d09126
BryanEstrada003 <mbestrad@espol.edu.ec>
2023-11-13 00:10:57 -0500

    mini ventna que permite recorrer contactos de manera circular

--- TestProject/src/main/java/com/mycompany/testproject/App.java
+++ TestProject/src/main/java/com/mycompany/testproject/App.java
@@ -3,21 +3,17 @@ package com.mycompany.testproject;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
 public class App extends Application {
-
-    private List<Image> imageList = new ArrayList<>();
-    private ListView<ImageView> listView = new ListView<>();
+    private ContactLibrary contactLibrary = new ContactLibrary();
+    private ImageView imageView;
+    private Button prevButton;
+    private Button nextButton;
 
     public static void main(String[] args) {
         launch(args);
@@ -25,32 +21,100 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Button uploadButton = new Button("Subir Imagen");
-        uploadButton.setOnAction(e -> openFileChooser());
+        // Interfaz de usuario
+        VBox root = new VBox();
+        root.setSpacing(10);
 
-        VBox vbox = new VBox(uploadButton, listView);
+        imageView = new ImageView();
+        imageView.setFitHeight(200); // ajusta la altura según sea necesario
+        imageView.setPreserveRatio(true);
 
-        Scene scene = new Scene(vbox, 400, 300);
-        primaryStage.setTitle("Image Uploader App");
+        prevButton = new Button("Anterior");
+        nextButton = new Button("Siguiente");
+
+        HBox buttonBox = new HBox(prevButton, nextButton);
+        buttonBox.setSpacing(10);
+
+        root.getChildren().addAll(imageView, buttonBox);
+
+        prevButton.setOnAction(e -> showPreviousContact());
+        nextButton.setOnAction(e -> showNextContact());
+
+        updateContactView();
+
+        Scene scene = new Scene(root, 400, 300);
+        primaryStage.setTitle("Contact Library App");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    private void openFileChooser() {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Imágenes", "*.png", "*.jpg", "*.gif"));
+    private void showPreviousContact() {
+        contactLibrary.movePrevious();
+        updateContactView();
+    }
 
-        File selectedFile = fileChooser.showOpenDialog(null);
+    private void showNextContact() {
+        contactLibrary.moveNext();
+        updateContactView();
+    }
 
-        if (selectedFile != null) {
-            Image image = new Image(selectedFile.toURI().toString());
-            imageList.add(image);
+    private void updateContactView() {
+        Contact currentContact = contactLibrary.getCurrentContact();
+        if (currentContact != null) {
+            imageView.setImage(new Image("file:"+currentContact.getImagePath()));
+            
+        }
+    }
 
-            ImageView imageView = new ImageView(image);
-            imageView.setFitWidth(100); // Ajusta el ancho según sea necesario
-            imageView.setPreserveRatio(true);
+    public static class Contact {
+        private String name;
+        private String phoneNumber;
+        private String imagePath;
 
-            listView.getItems().add(imageView);
+        public Contact(String name, String phoneNumber, String imagePath) {
+            this.name = name;
+            this.phoneNumber = phoneNumber;
+            this.imagePath = imagePath;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public String getImagePath() {
+            return imagePath;
+        }
+    }
+
+    public static class ContactLibrary {
+        private Contact[] contacts;
+        private int currentIndex;
+
+        public ContactLibrary() {
+            // Simulación de datos de contacto
+            contacts = new Contact[]{
+                    new Contact("Contacto 1", "123-456-7890", "C:\\Users\\HOME\\Desktop\\Data Structure Practices\\1P_DataStructure_G6\\TestProject\\src\\main\\resources\\images\\Screenshot 2023-10-24 194027.png"),
+                    new Contact("Contacto 2", "234-567-8901", "C:\\Users\\HOME\\Downloads\\Screenshot 2023-11-11 231152.png"),
+                    new Contact("Contacto 3", "345-678-9012", "C:\\Users\\HOME\\Downloads\\Picture1.png"),
+                    // Agrega más contactos según sea necesario
+            };
+            currentIndex = 0;
+        }
+
+        public void moveNext() {
+            currentIndex = (currentIndex + 1) % contacts.length;
+        }
+
+        public void movePrevious() {
+            currentIndex = (currentIndex - 1 + contacts.length) % contacts.length;
+        }
+
+        public Contact getCurrentContact() {
+            return contacts[currentIndex];
         }
     }
 }
