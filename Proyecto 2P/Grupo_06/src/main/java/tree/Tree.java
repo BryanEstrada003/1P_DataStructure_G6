aa91cd1b746b8b826cc05b1ea64682e26e01c4d8
BryanEstrada003 <mbestrad@espol.edu.ec>
2023-12-23 22:36:17 -0500

    Proyecto 2P

---
+++ Proyecto 2P/Grupo_06/src/main/java/tree/Tree.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tree;
+
+/**
+ *
+ * @author Michael Estrada
+ */
+public class Tree<E> {
+    
+    private TreeNode<E> root;
+    
+    public Tree () {
+        this.root = null; 
+    }
+    
+    public boolean isEmpty () {
+        return this.root == null;
+    }
+
+    public E getRoot() {
+        return root.getContent();
+    }
+    
+    private TreeNode getRootNode () {
+        return this.root;
+    }
+
+    private void setRootNode(TreeNode<E> root) {
+        this.root = root;
+    }
+    
+    public void setRoot (E content) {
+        this.root.setContent(content);
+    }
+    
+    public boolean isLeaf () {
+        return this.root.getChildren().isEmpty();
+    }
+    
+}
