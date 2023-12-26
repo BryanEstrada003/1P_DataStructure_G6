0b3acba5b91582f27d238b912296f72d6cf709dc
Luis R. <lromeror@fiec.espol.edu.ec>
2023-12-26 00:44:13 -0500

    TDA_Tree

---
+++ Group_06_2P/src/main/java/ec/edu/espol/TDAs/Tree.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.TDAs;
+
+public class Tree<E> {
+
+    private TreeNode<E> root;
+
+    public Tree() {
+        this.root = null;
+    }
+
+    public boolean isEmpty() {
+        return this.root == null;
+    }
+
+    public E[][] getRoot() {
+        return root.getContent();
+    }
+
+    private TreeNode getRootNode() {
+        return this.root;
+    }
+
+    public void setRoot(E[][] matriz) {
+        if (this.root == null) {
+            this.root = new TreeNode<>(matriz);
+        } else {
+            this.root.setContent(matriz);
+        }
+    }
+
+    public boolean isLeaf() {
+        return this.root.getChildren().isEmpty();
+    }
+
+    public void tableroEmpty() {
+        Integer[][] initialBoard = new Integer[3][3];
+
+        for (Integer[] initialBoard1 : initialBoard) {
+            for (int j = 0; j < initialBoard1.length; j++) {
+                initialBoard1[j] = -1; 
+            }
+        }
+//        this.setRoot();
+    }
+}
---
+++ Group_06_2P/src/main/java/ec/edu/espol/TDAs/TreeNode.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.TDAs;
+
+import java.util.LinkedList;
+import java.util.List;
+
+class TreeNode<E> {
+    private E  matriz[][];
+    private List<Tree<E>> children;
+
+    public TreeNode(E[][] matriz) {
+        this.matriz = matriz;
+        this.children = new LinkedList<>();
+    }
+
+    public E[][]  getContent() {
+        return this.matriz;
+    }
+
+    public void setContent(E[][] matriz) {
+        this.matriz = matriz;
+    }
+
+    public List<Tree<E>> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Tree<E>> children) {
+        this.children = children;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                sb.append(matriz[i][j] != null ? matriz[i][j].toString() : " ");
+                if (j < matriz[i].length - 1) {
+                    sb.append(" | ");
+                }
+            }
+            if (i < matriz.length - 1) {
+                sb.append("\n---------\n");
+            }
+        }
+        return sb.toString();
+    }
+}
